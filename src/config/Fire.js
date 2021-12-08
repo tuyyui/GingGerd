bb04ea0d1d42807d217c846e928e06428d21e312
Justin Young <mrjustinyoung991@gmail.com>
2021-12-08 13:21:07 -0600

    Update Fire.js

--- src/config/Fire.js
+++ src/config/Fire.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBlgi-SRpq9yjc4DCdmbbC_JWwSKy1b_KQ",
-    authDomain: "users-gerdwebsite.firebaseapp.com",
-    projectId: "users-gerdwebsite",
-    storageBucket: "users-gerdwebsite.appspot.com",
-    messagingSenderId: "883131266372",
-    appId: "1:883131266372:web:4500909a0751ad9068d311",
-    measurementId: "G-F3SYFBZXZJ"
+    apiKey: "",
+    authDomain: "m",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   // Initialize Firebase
   const fire = firebase.initializeApp(firebaseConfig);
